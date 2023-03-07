92118f0c64bd57262847e02ce7711ec5ea2d6c46
Voxuel <Leo.Fridh@hotmail.com>
2023-03-07 14:52:19 +0100

    add api call to json file

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const fetchDataCall = async ({ api }) => {
+    let apiReturn = await axios
+      .get(api)
+      .then(async function(response) {
+        return response;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    return apiReturn;
+  };
+
 function Portfolio(){
-    return(
+    const [data, setData] = useState({ work: [] });
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        console.log('hello from useEffect');
+        const fetchData = async () => {
+          setIsLoading(true);
+    
+          const result = await axios('cv.json');
+          console.log(result.data);
+          setData(result.data);
+          setIsLoading(false);
+        };
+    
+        fetchData();
+      }, []);
+      console.log('hey from portfolio');
+      return( 
+        <>
         <h1>Portfolio</h1>
+        {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {data.work.map(item => (
+            <li>
+              <h4>{item.name}</h4>
+              <p>{item.Role}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+        </>
     );
 }
+
 export default Portfolio;
